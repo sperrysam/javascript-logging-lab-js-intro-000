a32b6a1790f532f59b1d45469fa8cab848db14c6
Sam Perry <samperry88@googlemail.com>
2019-12-06 16:07:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-console.error("HALP!")
-console.log("I would be a logger.") // get it?
-console.warn()
+console.error("HALP!");
+console.log("I would be a logger."); // get it?
+console.warn("This is a warning!");
